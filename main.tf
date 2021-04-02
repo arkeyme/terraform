4db2050fc9aa859066b132c2fb363c1721a3cbfc
Ilya Taskaev <ansustiwaz@gmail.com>
2021-04-02 15:01:09 +0300

    Security group added and key_pair

--- main.tf
+++ main.tf
@@ -37,28 +37,62 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
 resource "aws_instance" "example" {
-  count = 4
+  count = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
+  key_name = "EC2_Tutor"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = join("",[var.instance_name,"-example-",count.index])
   }
 }
 
 resource "aws_instance" "web" {
-  count = 4
+  count = 2
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
+  key_name = "EC2_Tutor"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
     Name = join("",[var.instance_name,"-web-",count.index])
   }
 }
-## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+
+# ## https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/eip
+# resource "aws_eip" "lb" {
+#   for_each = toset(aws_instance.web.*.id)
+#   instance = each.key
+#   vpc = true
+#   }
+
 resource "aws_eip" "lb" {
   instance = aws_instance.web[0].id
   vpc = true
   }
+
 
