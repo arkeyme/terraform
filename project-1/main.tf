cc677ea6a843ad5c59b38be8f74bf4c3ade638f3
Ilya Taskaev <ansustiwaz@gmail.com>
2021-04-05 23:49:16 +0300

    vpc and subnet added

--- project-1/main.tf
+++ project-1/main.tf
@@ -18,10 +18,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-    count = 3
+    count = 1
     ami = "ami-02baf2b4223a343e8"
     instance_type = "t3.micro"
     tags = {
     Name = "amazon-linux"
   }
+}
+
+resource "aws_vpc" "first_vpc" {
+    cidr_block = "10.10.0.0/16"
+    enable_dns_hostnames = true
+    enable_dns_support = true
+    tags = {
+      "Name" = "Prod"
+    }
+}
+
+resource "aws_subnet" "name" {
+    vpc_id     = aws_vpc.first_vpc.id
+    cidr_block = "10.10.1.0/24"
+    tags = {
+    Name = "Prod_subnet"
+}
 }
