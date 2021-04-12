5da95f57bc7287898232d51085595bbe7fd91597
Ilya Taskaev <ansustiwaz@gmail.com>
2021-04-12 21:13:41 +0300

    VPC count reduced to one

--- Project-vpc-1/main.tf
+++ Project-vpc-1/main.tf
@@ -13,10 +13,32 @@ terraform {
   }
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 provider "aws" {
   profile = "default"
   region  = "eu-west-1"
 }
+
+### Start of VPC block #############
 
 resource "aws_vpc" "first_vpc" {
     cidr_block = "10.1.0.0/16"
@@ -24,15 +46,6 @@ resource "aws_vpc" "first_vpc" {
     enable_dns_support = true
     tags = {
       "Name" = "wordpress-vpc-1"
-    }
-}
-
-resource "aws_vpc" "second_vpc" {
-    cidr_block = "10.2.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
-    tags = {
-      "Name" = "wordpress-vpc-2"
     }
 }
 
@@ -46,9 +59,9 @@ resource "aws_subnet" "sbnt-vpc1" {
 }
 
 resource "aws_subnet" "sbnt-vpc2" {
-    vpc_id     = aws_vpc.second_vpc.id
+    vpc_id     = aws_vpc.first_vpc.id
     availability_zone = data.aws_availability_zones.available.names[1]
-    cidr_block = "10.2.2.0/24"
+    cidr_block = "10.1.2.0/24"
     tags = {
     Name = "wordpress-sbnt-2"
 }
@@ -58,13 +71,6 @@ resource "aws_internet_gateway" "gw1" {
     vpc_id = aws_vpc.first_vpc.id
     tags = {
     Name = "gw1"
-  }
-}
-
-resource "aws_internet_gateway" "gw2" {
-    vpc_id = aws_vpc.second_vpc.id
-    tags = {
-    Name = "gw2"
   }
 }
 
@@ -80,18 +86,6 @@ resource "aws_route_table" "rt1" {
   }
 }
 
-resource "aws_route_table" "rt2" {
-  vpc_id = aws_vpc.second_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw2.id
-  }
-  tags = {
-    Name = "rt2"
-  }
-}
-
 resource "aws_route_table_association" "a" {
     subnet_id      = aws_subnet.sbnt-vpc1.id
     route_table_id = aws_route_table.rt1.id
@@ -99,5 +93,69 @@ resource "aws_route_table_association" "a" {
 
 resource "aws_route_table_association" "b" {
     subnet_id      = aws_subnet.sbnt-vpc2.id
-    route_table_id = aws_route_table.rt2.id
+    route_table_id = aws_route_table.rt1.id
 }
+
+### Start of EC2 block #############
+
+resource "aws_security_group" "allow_ssh_wordpress-1" {
+  name        = "allow_ssh_1"
+  description = "Allow ssh inbound traffic"
+  vpc_id     = aws_vpc.first_vpc.id
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_1"
+  }
+}
+
+resource "aws_instance" "wordpress-1" {
+  count = 1
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = "EC2_Tutor"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_wordpress-1.id]
+  subnet_id   = aws_subnet.sbnt-vpc1.id
+  tags = {
+    Name = join("",[var.instance_name,"-wordpress-1"])
+  }
+}
+
+
+resource "aws_instance" "wordpress-2" {
+  count = 1
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  key_name = "EC2_Tutor"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_wordpress-1.id]
+  subnet_id   = aws_subnet.sbnt-vpc2.id
+
+  tags = {
+    Name = join("",[var.instance_name,"-wordpress-1"])
+  }
+}
+
+resource "aws_eip" "eip1" {
+  instance = aws_instance.wordpress-1[0].id
+  vpc = true
+  }
+
+resource "aws_eip" "eip2" {
+  instance = aws_instance.wordpress-2[0].id
+  vpc = true
+  }
+
+
